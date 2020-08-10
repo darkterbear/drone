80b62f14de1ce20649a1d0e9d6640b2889402e03
Terrance Li <tyli@berkeley.edu>
2020-08-10 12:37:23 -0700

    Fixes for raw throttle + yaw inputs

--- raspberry-pi/test-bluetooth.py
+++ raspberry-pi/test-bluetooth.py
@@ -3,11 +3,11 @@ import time
 leftStick = BlueDot()
 
 while True:
-    x, y = 0.0, 0.0
+    x, y = 0.0, -1.0
     if leftStick.is_pressed:
         x, y = leftStick.position.x, leftStick.position.y
     throttle = (y + 1.0) / 2.0
     rudder = x ** 3
 
-    print('throttle: ' + throttle + '\t rudder: ' + rudder)
+    print('throttle: ' + str(throttle) + '\t rudder: ' + str(rudder))
     time.sleep(0.05)
