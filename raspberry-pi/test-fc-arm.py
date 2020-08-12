704e1108ad92d2c46e15152501ff9f2da39558fd
Terrance Li <tyli@berkeley.edu>
2020-08-12 11:29:24 -0700

    Added RC to fc-arm test

--- raspberry-pi/test-fc-arm.py
+++ raspberry-pi/test-fc-arm.py
@@ -13,40 +13,55 @@ print('Armed')
 time.sleep(1.0)
 
 buf = []
-push16(buf, 2000)
-push16(buf, 2000)
-push16(buf, 2000)
-push16(buf, 2000)
-push16(buf, 2000)
-push16(buf, 2000)
-push16(buf, 2000)
-push16(buf, 2000)
+for _ in range(8):
+    push16(buf, 2000)
 board.sendCMD(MultiWii.SET_MOTOR, buf)
 
 print('Motors HIGH')
 time.sleep(5.0)
 
 buf = []
-push16(buf, 1500)
-push16(buf, 1500)
-push16(buf, 1500)
-push16(buf, 1500)
-push16(buf, 1500)
-push16(buf, 1500)
-push16(buf, 1500)
-push16(buf, 1500)
+for _ in range(8):
+    push16(buf, 1500)
 board.sendCMD(MultiWii.SET_MOTOR, buf)
 
 print('Motors MEDIUM')
 time.sleep(5.0)
 
+buf = []
+for _ in range(8):
+    push16(buf, 1000)
+board.sendCMD(MultiWii.SET_MOTOR, buf)
+
+print('Motors LOW')
+time.sleep(5.0)
+
+buf = []
+push16(buf, 1500)
+push16(buf, 1500)
+push16(buf, 2000)
+push16(buf, 1500)
+push16(buf, 1000)
+push16(buf, 1000)
+push16(buf, 1000)
+push16(buf, 1000)
+board.sendCMD(MultiWii.SET_RAW_RC, buf)
+print('RC Throttle HIGH')
+time.sleep(5.0)
+
+buf = []
+push16(buf, 1500)
+push16(buf, 1500)
+push16(buf, 1000)
+push16(buf, 1500)
+push16(buf, 1000)
+push16(buf, 1000)
+push16(buf, 1000)
+push16(buf, 1000)
+board.sendCMD(MultiWii.SET_RAW_RC, buf)
+print('RC Throttle LOW')
+time.sleep(5.0)
+
 board.disarm()
 print('Disarmed')
 time.sleep(1.0)
-
-# while True:
-#     board.sendCMD(16, MultiWii.SET_RAW_RC, [
-#                   1500, 1500, 1800, 1500, 2000, 1000, 1000, 1000], '8H')
-#     time.sleep(0.05)
-#     print(board.getData(MultiWii.MOTOR))
-#     time.sleep(0.05)
