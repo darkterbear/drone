4f866a092345d678fbf2f759e78adc8bb5eca4bd
Terrance Li <tyli@berkeley.edu>
2020-08-12 13:07:59 -0700

    Add prearming sequence

--- raspberry-pi/test-fc-rc.py
+++ raspberry-pi/test-fc-rc.py
@@ -10,7 +10,20 @@ time.sleep(1.0)
 
 board.arm()
 print('Armed')
-time.sleep(1.0)
+time.sleep(5.0)
+
+for _ in range(20):
+    buf = []
+    push16(buf, 1500)
+    push16(buf, 1500)
+    push16(buf, 1000)
+    push16(buf, 1500)
+    push16(buf, 1000)
+    push16(buf, 1000)
+    push16(buf, 1000)
+    push16(buf, 1000)
+    board.sendCMD(MultiWii.SET_RAW_RC, buf)
+    time.sleep(0.05)
 
 while True:
     buf = []
