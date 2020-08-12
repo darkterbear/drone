046cca4fa2deea2f7ec6a351bd429a299bc22740
Terrance Li <tyli@berkeley.edu>
2020-08-12 13:25:09 -0700

    Reorganize testing scripts

--- raspberry-pi/test-fc-telemetry.py
+++ raspberry-pi/test-fc-telemetry.py
@@ -1,14 +1,8 @@
 #!/usr/bin/env python
 
 from msp import MultiWii
-from sys import stdout
 
-if __name__ == "__main__":
-
-    board = MultiWii("/dev/ttyACM0")
-    try:
-        while True:
-            board.getData(MultiWii.ATTITUDE)
-            print(board.attitude)
-    except Exception as error:
-        print("Error on Main: " + str(error))
+board = MultiWii("/dev/ttyACM0")
+while True:
+    board.getData(MultiWii.ATTITUDE)
+    print(board.attitude)
